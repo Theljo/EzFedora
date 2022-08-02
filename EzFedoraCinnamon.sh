1cfbee70b61f1a66eed6532e3d8004b2eec90a33
Theljo <102119717+Theljo@users.noreply.github.com>
2022-08-02 19:53:25 -0400

    Update EzFedoraCinnamon.sh

--- EzFedoraCinnamon.sh
+++ EzFedoraCinnamon.sh
@@ -1,22 +1,23 @@
 #update + gui
-echo "updating + gui"
+echo"updating + gui"
 dnf -y update
 sudo dnf -y group install "Cinnamon Desktop"
 #adding apps
-echo "adding apps"
+echo"adding apps"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 dnf install dnf-plugins-core
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 dnf install -y brave-browser
-sudo flatpak install flathub com.spotify.Client
+flatpak -y install flathub com.spotify.Client
 flatpak -y install flathub com.discordapp.Discord
 flatpak -y install flathub com.visualstudio.code
 flatpak -y install flathub com.obsproject.Studio
 flatpak -y install flathub org.gimp.GIMP
 flatpak -y install flathub com.usebottles.bottles
-
+flatpak install flathub org.kde.kdenlive
+dnf install kitty
 
 #finish up
-echo "reboot"
+echo"reboot"
 reboot
