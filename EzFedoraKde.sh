c89cccedd1a3fb15b7e03f1c17851dc994234966
Theljo <102119717+Theljo@users.noreply.github.com>
2022-08-02 19:55:19 -0400

    Update EzFedoraKde.sh

--- EzFedoraKde.sh
+++ EzFedoraKde.sh
@@ -9,15 +9,15 @@ dnf install dnf-plugins-core
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 dnf install -y brave-browser
-sudo flatpak install flathub com.spotify.Client
+flatpak -y install flathub com.spotify.Client
 flatpak -y install flathub com.discordapp.Discord
 flatpak -y install flathub com.visualstudio.code
 flatpak -y install flathub com.obsproject.Studio
 flatpak -y install flathub org.gimp.GIMP
 flatpak -y install flathub com.usebottles.bottles
-
+flatpak install flathub org.kde.kdenlive
+dnf install kitty
 
 #finish up
 echo "reboot"
 reboot
-
